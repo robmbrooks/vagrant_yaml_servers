c7c079cd81aa25e279e6b90d5bdf77b44cea7b27
Robert Brooks <robert.brooks@riskiq.net>
2017-11-21 11:58:45 -0800

    fix requires

--- lib/vagrant_yaml_servers.rb
+++ lib/vagrant_yaml_servers.rb
@@ -7,4 +7,8 @@ rescue LoadError
   raise 'The vagrant_yaml_servers plugin must be run within Vagrant.'
 end
 
+require "vagrant_yaml_servers/command"
+require "vagrant_yaml_servers/config"
+require "vagrant_yaml_servers/plugin"
+require "vagrant_yaml_servers/version"
 require "yaml_servers"
